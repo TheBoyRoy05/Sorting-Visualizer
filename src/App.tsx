89c0221f61260eefd7b390d424b774dc7390e9a2
Issac Roy <issacroy05@gmail.com>
2024-07-03 15:22:18 -0700

    Got a generate button + slider for the elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,79 @@
-import { FC } from "react";
-
+import { FC, useEffect, useState } from "react";
 
 interface BarProps {
+  width: number;
   height: number;
   index: string;
 }
 
-const Bar: FC<BarProps> = ({ height, index }) => {
+const Bar: FC<BarProps> = ({ width, height, index }) => {
+  const style = {
+    height: `${height}px`,
+    width: `${width}px`,
+  };
   return (
-    <div className="bar" id={index} style={{ height: `${height}px` }}>
-      {height < 100 ? height : ""}
+    <div className="bar" id={index} style={style}>
+      <p className="bar-text">{width > 20 ? height : ""}</p>
     </div>
   );
 };
 
-const App: FC = () => {
+interface DisplayProps {
+  heights: number[];
+}
+
+const Display: FC<DisplayProps> = ({ heights }) => {
   return (
-    <div className="app">
-      <Bar height={20} index={"0"} />
+    <div className="display">
+      {heights.map((height, index) => (
+        <Bar key={index} width={21} height={height} index={index.toString()} />
+      ))}
     </div>
   );
+};
+
+interface CustomButtonProps {
+  text: string;
+  handleClick: () => void;
+}
+
+const CustomButton: FC<CustomButtonProps> = ({ text, handleClick }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const App: FC = () => {
+  const [numElems, setNumElems] = useState(10);
+  const [heights, setHeights] = useState<number[]>([]);
+
+  const generate = (numElems: number) => {
+    setHeights(Array.from({ length: numElems }, () => getRandomInt(1, 100)));
+  };
+
+  useEffect(() => {
+    generate(numElems);
+  }, [numElems]);
+
+  return (
+    <div className="app">
+      <CustomButton text="Generate" handleClick={() => generate(numElems)} />
+      <div className="slider">
+        <input
+          type="range"
+          onChange={(e) => setNumElems(Number(e.target.value))}
+          min="2"
+          max="100"
+          step="1"
+          value={numElems}
+        />
+        <p className="slider-value">{numElems}</p>
+      </div>
+      <Display heights={heights} />
+    </div>
+  );
+};
+
+const getRandomInt = (min: number, max: number): number => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 export default App;
